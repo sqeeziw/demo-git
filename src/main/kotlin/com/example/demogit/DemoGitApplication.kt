dc4af3be9d4d2cac9032c06457fe2899b1e6a1f5
sqeeziw <newvlad@internet.ru>
2026-04-15 15:06:25 +0500

    init commit

---
+++ src/main/kotlin/com/example/demogit/DemoGitApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demogit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoGitApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoGitApplication>(*args)
+}
